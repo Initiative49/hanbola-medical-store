5748d86c76539cc209a3157da679e2b7b98cb213
Initiative49 <initiative49@gmail.com>
2026-09-11 06:49:08 +0300

    Update store-data.js

--- data/store-data.js
+++ data/store-data.js
@@ -4,9 +4,9 @@ const categories=[
 {id:"bp-monitors",name:"أجهزة الضغط الطبية",image:"cat-002.jpg"},
 {id:"accessories",name:"الإكسسوارات الطبية",image:"cat-003.jpg"}];
 const products=[
-{id:1,name:"سماعات MDF الكلاسيكية",category:"stethoscopes",price:9.50,image:1,featured:true,description:"سماعة طبية مناسبة للاستخدام اليومي والعيادات."},
-{id:2,name:"سماعات MDF التخصصية",category:"stethoscopes",price:6.50,image:2,featured:true,description:"تصميم عملي وخفيف للاستخدام الطبي."},
-{id:3,name:"جهاز ضغط MDF",category:"bp-monitors",price:13.95,image:3,featured:true,description:"جهاز رقمي سهل الاستخدام لقياس ضغط الدم."},
-{id:4,name:"أجهزة ضغط Exanovo ",category:"bp-monitors",price:11.40,image:4,featured:false,description:"جهاز ضغط عملي للاستخدام المنزلي."},
-{id:5,name:"الثرمومتر الطبي",category:"accessories",price:3.15,image:5,featured:true,description:"قفازات طبية للاستخدامات اليومية."},
-{id:6,name:"أجهزة Pulse Oximeter",category:"accessories",price:2.20,image:6,featured:false,description:"كمامات طبية للاستخدام اليومي."}];
+{id:1,name:"سماعات MDF الكلاسيكية",category:"stethoscopes",price:9.50,image:1,featured:true,description:"سماعة طبية مناسبة للاستخدام اليومي."},
+{id:2,name:"سماعات MDF التخصصية",category:"stethoscopes",price:6.50,image:2,featured:true,description:"سماعات الCardiology الفخمة تفيد للدراسات التخصصية."},
+{id:3,name:"جهاز ضغط MDF",category:"bp-monitors",price:13.95,image:3,featured:true,description:"من أفضل أجهزة الضغط ذات الكفاءة العالية والصلابة."},
+{id:4,name:"أجهزة ضغط Exanovo ",category:"bp-monitors",price:11.40,image:4,featured:false,description:"أجهزة ضغط عملية سلسة ذو منفذ واحد."},
+{id:5,name:"الثرمومتر الطبي",category:"accessories",price:3.15,image:5,featured:true,description:"ممتاز لقياس درجة حرارة المريض وغير قابل للكسر نظرا لطرفه المرن."},
+{id:6,name:"أجهزة Pulse Oximeter",category:"accessories",price:2.20,image:6,featured:false,description:"لقياس تشبع الاوكسجين في الدم ونبضات القلب ذو جودة عالية."}];
