0358b71c2cace7143589271969115697cf2397f7
Initiative49 <initiative49@gmail.com>
2026-09-13 12:48:10 +0300

    Update store-data.js

--- data/store-data.js
+++ data/store-data.js
@@ -4,9 +4,9 @@ const categories=[
 {id:"bp-monitors",name:"أجهزة الضغط الطبية",image:"cat-002.jpg"},
 {id:"accessories",name:"الإكسسوارات الطبية",image:"cat-003.jpg"}];
 const products=[
-{id:1,name:"سماعات MDF الكلاسيكية",category:"stethoscopes",price:22,image:1,featured:true,description:"سماعة طبية مناسبة للاستخدام اليومي."},
+{id:1,name:"سماعات MDF الكلاسيكية",category:"stethoscopes",price:22,image:1,featured:true,bestSeller:true,description:"سماعة طبية مناسبة للاستخدام اليومي."},
 {id:2,name:"سماعات MDF التخصصية",category:"stethoscopes",price:47.50,image:2,featured:true,description:"سماعات الCardiology الفخمة تفيد للدراسات التخصصية."},
 {id:3,name:"جهاز ضغط MDF",category:"bp-monitors",price:34.85,image:3,featured:true,description:"من أفضل أجهزة الضغط ذات الكفاءة العالية والصلابة."},
-{id:4,name:"أجهزة ضغط Exanovo ",category:"bp-monitors",price:17,image:4,featured:false,description:"أجهزة ضغط عملية سلسة ذو منفذ واحد."},
+{id:4,name:"أجهزة ضغط Exanovo ",category:"bp-monitors",price:17,image:4,featured:false,bestSeller:true,description:"أجهزة ضغط عملية سلسة ذو منفذ واحد."},
 {id:5,name:"الثرمومتر الطبي",category:"accessories",price:1.899,image:5,featured:true,description:"ممتاز لقياس درجة حرارة المريض وغير قابل للكسر نظرا لطرفه المرن."},
 {id:6,name:"أجهزة Pulse Oximeter",category:"accessories",price:9.50,image:6,featured:false,description:"لقياس تشبع الاوكسجين في الدم ونبضات القلب ذو جودة عالية."}];
