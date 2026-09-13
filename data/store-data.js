9cf0e97e37c3943f214978eeace1dad2b4b54dc0
Initiative49 <initiative49@gmail.com>
2026-09-13 13:15:45 +0300

    Update store-data.js

--- data/store-data.js
+++ data/store-data.js
@@ -4,16 +4,10 @@ const categories=[
 {id:"bp-monitors",name:"أجهزة الضغط الطبية",image:"cat-002.jpg"},
 {id:"accessories",name:"الإكسسوارات الطبية",image:"cat-003.jpg"}];
 const products=[
-{id:1,name:"MDF 747 - Dual Head Stethoscope",category:"stethoscopes",price:22,image:1,featured:true,bestSeller:true,description:"سماعة MDF®️ Dual Head هي أداة تشخيصية خفيفة الوزن، توفر صوتيات حساسة وأداءً احترافيًا بسعر مناسب. ويضمن تصميمها المريح راحة الطبيب والمريض، حتى عند استخدامها لفترات طويلة. كما أنها مصنوعة يدويًا بدقة، مما يجعلها خيارًا موثوقًا لدى المتخصصين في الرعاية الصحية.
-قطعة صدرية ثنائية الرأس مصنوعة من الألمنيوم خفيف الوزن وعالي المتانة، ومصنّعة بدقة ومصقولة يدويًا للحصول على أعلى مستوى من الأداء والمتانة.
-ساق صمام صوتي دوّارة بالكامل مزودة بنقطة مؤشر خضراء، تساعد المستخدم على تحديد قناة الصوت النشطة بسهولة.
-مزودة بغشاء ليفي فائق الرقة مصمم لاكتشاف الأصوات ذات الترددات العالية.
-أما الجرس (Bell)، والمزوّد بحلقة غير باردة لراحة المريض، فيوفر تضخيمًا أفضل للصوت، وعزلًا للترددات المنخفضة، مع الحفاظ على راحة المريض أثناء الفحص."},
+{id:1,name:"MDF 747 - Dual Head Stethoscope",category:"stethoscopes",price:22,image:1,featured:true,bestSeller:true,description:""},
 {id:2,name:"سماعات MDF التخصصية",category:"stethoscopes",price:47.50,image:2,featured:true,description:"سماعات الCardiology الفخمة تفيد للدراسات التخصصية."},
 {id:3,name:"جهاز ضغط MDF",category:"bp-monitors",price:34.85,image:3,featured:true,description:"من أفضل أجهزة الضغط ذات الكفاءة العالية والصلابة."},
 {id:4,name:"أجهزة ضغط Exanovo ",category:"bp-monitors",price:17,image:4,featured:false,bestSeller:true,description:"أجهزة ضغط عملية سلسة ذو منفذ واحد."},
 {id:5,name:"الثرمومتر الطبي",category:"accessories",price:1.899,image:5,featured:true,description:"ممتاز لقياس درجة حرارة المريض وغير قابل للكسر نظرا لطرفه المرن."},
 {id:6,name:"أجهزة Pulse Oximeter",category:"accessories",price:9.50,image:6,featured:false,description:"لقياس تشبع الاوكسجين في الدم ونبضات القلب ذو جودة عالية."},
-{id:7,name:"MDF MD One®️ Stainless Steel Dual Head",category:"stethoscopes",price:25,image:7,featured:false,bestSeller:true,description:"هي سماعة عالية الجودة، متينة وكلاسيكية، يعتمد عليها الممرضون والأطباء والممارسون الصحيون والمسعفون والطلاب وغيرهم منذ سنوات.
-تتميز قطعة الصدر ذات الجرس الحقيقي والغشاء الحقيقي بقدرتها على التقاط مجموعة واسعة من الأصوات ذات الترددات المنخفضة والعالية. وهي مصنوعة من الفولاذ المقاوم للصدأ عالي الجودة، مع أنابيب طويلة وكثيفة تساعد على حجب الضوضاء المحيطة، وأطراف أذن ناعمة من السيليكون تعمل على إحكام عزل الصوت، بالإضافة إلى قطعة صدر مصقولة بلمسة نهائية عالية الجودة أشبه بتشطيب المجوهرات.
-قد تبدو سماعة MD One®️ مشابهة لغيرها من السماعات، لكن حرصنا على جودة المواد والاهتمام بأدق التفاصيل هو ما يجعلها مختلفة ومميزة."}];
+{id:7,name:"MDF MD One®️ Stainless Steel Dual Head",category:"stethoscopes",price:25,image:7,featured:false,bestSeller:true,description:""}];
